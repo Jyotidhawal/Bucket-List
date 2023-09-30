a36223c352b84799ee55336181e6c05f39d19515
princeninama <princeninama254@gmail.com>
2023-09-30 17:12:20 +0530

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,18 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-
-      <div className="bg-black  h-12 fixed py-3 w-full space-x-5 text-white z-10">
-        <select name="Idea" id="idea" className="bg-black">
-          <option value="Bucket List">Bucket List</option>
-          <option value="Bucket List">Done List</option>
-          <option value="Bucket List">Impossible List</option>
-        </select>
-        {/* <Link to="/about_us">About Us</Link>
-      <Link to="/sign_in">Sign In</Link>
-      <Link to="/sign_up">Sign up</Link> */}
+      <div className="bg-black w-full">
+        <div className="bg-black pl-[53rem] h-12 fixed py-3 w-full space-x-5 text-white z-10">
+          <a href="/about_us">Home</a>
+          <select name="Idea" id="idea" className="bg-black">
+            <option value="Bucket List">Bucket List</option>
+            <option value="Bucket List">Done List</option>
+            <option value="Bucket List">Impossible List</option>
+          </select>
+          <a href="/sign_in">About Us</a>
+          <a href="/sign_in">Sign In</a>
+          <a href="/sign_up">Sign up</a>
+        </div>
       </div>
     </>
   );
